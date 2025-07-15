74dd3a1c38f2834fcab7f6d32e3397ebbfa757b7
Vijay Iyer <iyervijay99@gmail.com>
2025-07-15 10:59:19 +0200

    icon update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // add this line to enable dark mode toggling via a class
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],

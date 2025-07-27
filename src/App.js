08634eec1129446dd39c6faaa8fae8d4983c9d51
Vijay Iyer <iyervijay99@gmail.com>
2025-07-27 23:15:42 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -875,7 +875,7 @@ function CarAccidentDetail() {
           href="https://github.com/iyervijay21/car-accident-alert-system"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200 hover:bg-purple-600 hover:text-white transition always-aura"
+          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200  hover:text-white transition always-aura"
         >
           <svg
             className="w-5 h-5 mr-2 fill-current"
@@ -946,7 +946,7 @@ function SelfDrivingDetail() {
           href="https://github.com/iyervijay21/3d-object-detection-using-LIDAR-for-self-driving-car"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200 hover:bg-purple-600 hover:text-white transition always-aura"
+          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200  hover:text-white transition always-aura"
 
         >
           <svg
@@ -1020,7 +1020,7 @@ function CamCussionDetail() {
           href="https://github.com/iyervijay21/iyervijay21-Camcussion-eye-tracking-software-for-concussion-diagnosis-"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200 hover:bg-purple-600 hover:text-white transition always-aura"
+          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200  hover:text-white transition always-aura"
 
         >
           <svg
@@ -1099,7 +1099,7 @@ function DeepFakeDetail() {
           href="https://github.com/iyervijay21/Deepfake_Detection"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200 hover:bg-purple-600 hover:text-white transition always-aura"
+          className="inline-flex items-center px-5 py-3 border border-purple-400 rounded-xl text-purple-200  hover:text-white transition always-aura"
 
         >
           <svg className="w-5 h-5 mr-2 fill-current" viewBox="0 0 24 24">
